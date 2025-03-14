7c22eb2f7b38d3f274935e66a25fc8cd2cf1f4f1
Alexis Vasquez <vasquez.a712@gmail.com>
2025-03-14 09:54:34 -0400

    audio_utils.h
    Included header file audio_utils.h for audio_utils.cpp file

---
+++ audio_utils.h
@@ -0,0 +1,12 @@
+#ifndef AUDIO_UTILS_H
+#define AUDIO_UTILS_H
+
+// Include necessary libraries
+#include <portaudio.h>
+#include <iostream>
+
+// Function declarations
+bool initializeAudio();
+void shutdownAudio();
+
+#endif // AUDIO_UTILS_H
