f6cded6c69c92e48453f60a06821bde09fa41c4c
alexisvassquez <vasquez.a712@gmail.com>
2026-04-23 13:18:46 -0400

    fixed the docstring formatting in feature_extraction.py

--- audio/ai/modules/feature_extraction.py
+++ audio/ai/modules/feature_extraction.py
@@ -2,8 +2,15 @@
 # AudioMIX
 # audio/ai/modules/feature_extraction.py
 
-# This module provides functions to extract audio features from music files using the librosa library.
-# It includes features such as MFCCs, spectral contrast, spectral bandwidth, and tempo. The extracted features are returned in a structured format for use in machine learning models or other applications. The module also includes verbose logging to provide insights into the feature extraction process and the time taken for each step.
+"""
+This module provides functions to extract audio 
+features from music files using the librosa library.
+It includes features such as MFCCs, spectral
+contrast, spectral bandwidth, and tempo. 
+The extracted features are returned in a structured
+format for use in machine learning models or other
+applications. The module also includes verbose logging to provide insights into the feature extraction process and the time taken for each step.
+"""
 
 import librosa
 import numpy as np
